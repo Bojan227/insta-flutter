e75d7d6e490c992d212a959b9f29104895a5b88b
Bojan227 <blazevski.bojan1@gmail.com>
2023-07-25 15:08:53 +0200

    drawer styling

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -13,27 +13,51 @@ class MainDrawer extends StatelessWidget {
         getIt<SharedPreferencesConfig>().getString('accessToken');
 
     return Drawer(
-        child: Column(
-      children: [
-        const DrawerHeader(
-          child: Text('Pettygram'),
-        ),
-        TextButton(
-          onPressed: () {
-            if (token != null) Navigator.of(context).pop();
+        elevation: 3,
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color(0xffF58529),
+            Color(0xffDD2A7B),
+            Color(0xff8134AF),
+            Color(0xff515BD4)
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Pettygram',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                  'Login',
+                ),
+                onTap: () {
+                  if (token != null) Navigator.of(context).pop();
 
-            context.go('/login');
-          },
-          child: const Text('Login'),
-        ),
-        TextButton(
-          onPressed: () {
-            getIt<SharedPreferencesConfig>().removeString('accessToken');
-            context.go('/login');
-          },
-          child: const Text('Logout'),
-        )
-      ],
-    ));
+                  context.go('/login');
+                },
+              ),
+              ListTile(
+                title: const Text(
+                  'Logout',
+                ),
+                onTap: () {
+                  getIt<SharedPreferencesConfig>().removeString('accessToken');
+                  context.go('/login');
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
