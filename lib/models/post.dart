b6e6c73835fefbedaeb188f28feda62ebc493c71
Bojan227 <blazevski.bojan1@gmail.com>
2023-07-27 13:50:08 +0200

    add post Model

---
+++ lib/models/post.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+
+class Post extends Equatable {
+  Post(
+      {this.id,
+      required this.text,
+      required this.createdBy,
+      required this.imageUrl,
+      required this.createdAt});
+
+  Post.fromJson(Map<String, dynamic> json) {
+    id = json['_id'];
+    text = json['text'];
+    createdBy = json['createdBy'];
+    imageUrl = json['imageUrl'];
+    imageId = json['imageId'];
+    createdAt = json['createdAt'];
+    likes = json['likes'];
+  }
+
+  String? id;
+  String? text;
+  Map<String, dynamic>? createdBy;
+  List<dynamic>? imageUrl;
+  List<dynamic>? imageId;
+  String? createdAt;
+  List<dynamic>? likes;
+
+  @override
+  List<Object?> get props => [id, text, createdBy, imageUrl];
+}
