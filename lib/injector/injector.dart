8b7c05e40123a6d150e885b138cc39bf1b9abf36
Bojan227 <blazevski.bojan1@gmail.com>
2023-07-27 13:49:26 +0200

    register userPostBloc

--- lib/injector/injector.dart
+++ lib/injector/injector.dart
@@ -4,6 +4,7 @@ import 'package:pettygram_flutter/api/pettygram_repo_impl.dart';
 import 'package:pettygram_flutter/app_config.dart';
 import 'package:pettygram_flutter/blocs/login/login_bloc.dart';
 import 'package:pettygram_flutter/blocs/user/user_bloc.dart';
+import 'package:pettygram_flutter/blocs/user_posts/user_post_bloc.dart';
 import 'package:pettygram_flutter/storage/shared_preferences.dart';
 
 GetIt getIt = GetIt.instance;
@@ -25,4 +26,8 @@ Future setupInjector(AppConfig config) async {
   final LoginBloc loginBloc = LoginBloc(
       storageConfig: sharedConfig, pettygramRepository: pettygramRepository);
   getIt.registerSingleton<LoginBloc>(loginBloc);
+
+  final UserPostBloc userPostsBloc =
+      UserPostBloc(pettygramRepository: pettygramRepository);
+  getIt.registerSingleton<UserPostBloc>(userPostsBloc);
 }
